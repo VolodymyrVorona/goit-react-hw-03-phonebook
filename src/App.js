5499a7898ca193c6a641ab238def2b9280d41d02
VolodymyrVorona <volodimir.vorona@gmail.com>
2021-06-16 14:11:53 +0300

    added project settings

--- src/App.js
+++ src/App.js
@@ -1,11 +1,80 @@
-import "./App.css";
+import { Component } from 'react';
+import contacts from './contacts.json';
+import shortId from 'shortid';
 
-function App() {
-  return (
-    <div className="App">
-      <p>Hallo</p>
-    </div>
-  );
+import ContactForm from './Components/ContactForm';
+import ContactsList from './Components/ContactList';
+import Filter from './Components/Filter';
+
+import st from './App.module.css';
+
+class App extends Component {
+  state = {
+    contacts,
+    filter: '',
+  };
+
+  addContact = ({ name, number }) => {
+    const newContact = {
+      id: shortId.generate(),
+      name,
+      number,
+    };
+
+    if (this.findTheSameName(name)) {
+      return alert(`${name} is already in contacts`);
+    }
+
+    this.setState(({ contacts }) => ({
+      contacts: [newContact, ...contacts],
+    }));
+  };
+
+  findTheSameName = newName => {
+    const { contacts } = this.state;
+    const normalizedName = newName.toLowerCase();
+
+    return contacts.find(({ name }) => name.toLowerCase() === normalizedName);
+  };
+
+  getVisibleContacts = () => {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter),
+    );
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  render() {
+    const { filter } = this.state;
+
+    const filteredСontacts = this.getVisibleContacts();
+
+    return (
+      <div className={st.wrapper}>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.addContact} />
+
+        <h2>Contacts</h2>
+        <Filter onContactsFilter={this.changeFilter} value={filter} />
+        <ContactsList
+          contacts={filteredСontacts}
+          onDeleteContact={this.deleteContact}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
